669b23c9b6377503cebb7b0146d3eb7355a28011
Thomas Weyssow <tweyssow@ulb.ac.be>
2020-12-21 12:39:57 +0100

    Initial Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import LoginForm from './components/LoginForm'
+import './App.css';
+
+function App() {
+
+    const adminUser = {
+        email: "admin@admin.com", 
+        password: "admin"
+    }
+
+    const [user, setUser] = useState({name: "", email: ""});
+    const [error, setError] = useState("");
+
+    const Login = details => {
+        if (details.email == adminUser.email && details.password == adminUser.password) {
+            console.log("Logged in");
+            setUser({
+                name: details.name, 
+                email: details.email
+            })
+        } else {
+            setError("Details do not match!");
+        }
+    }
+
+    const Logout = () => {
+        setUser({name: "", email: ""});
+    }
+
+    return (
+        <div className="app">
+            {(user.email != "") ? (
+                <div className="welcome">
+                    <h2>Welcome, <span>{user.name}</span></h2>
+                    <button onClick={Logout}>Logout</button>
+                </div>
+            ) : (
+                <LoginForm Login={Login} error={error}/>
+            )}
+        </div>
+    );
+    
+}
+
+export default App;
